ce4b7e282f9616389cac4e8e48dfd0587f77dfab
96olbap <alvinwanjala@gmail.com>
2022-05-20 00:47:47 +0300

    Template updates

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import InputRequired
 
 
@@ -10,3 +10,18 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us a little about you.',validators = [InputRequired()])
     submit = SubmitField('Submit')
 
+class PostUpdate(FlaskForm):
+    '''
+    PostUpdate class to create instances of update objects
+    '''
+    content = TextAreaField('Write your update post', validators= [InputRequired()])
+    author = TextAreaField('Name of author', validators = [InputRequired()])
+    category = SelectField('Area', choices=(['', 'CBD', 'Moi Avenue', 'Kenyatta Avenue', 'Ngara', 'Pangani', 'Kamukuji', 'Jogoo Road', 'Westlands', 'Other']), validators = [InputRequired()])
+    submit = SubmitField('Post')
+
+class PostComment(FlaskForm):
+    '''
+    PostComment class to create instances of comment obj
+    '''
+    content = TextAreaField('Write a comment', validators = [InputRequired()])
+    submit = SubmitField('Post')
